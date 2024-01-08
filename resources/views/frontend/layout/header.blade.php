7a6ba0f3c25c39f567978fb52427045e1f8da8f2
yuzgec <olcayy@gmail.com>
2024-01-08 04:35:20 +0300

    Fix

--- resources/views/frontend/layout/header.blade.php
+++ resources/views/frontend/layout/header.blade.php
@@ -3,13 +3,13 @@
         <div class="row nav-wrapper">
             <div class="col">
                 <a href="{{ route('home')}}" class="logo">
-                    <img src="/denden_logo_b.png" alt="website logo" class="logo-white" >
+                    <img src="/denden_logo_b.png" alt="DENDEN DENİZCİLİK" class="logo-white" >
                 </a>
             </div>
 
             <div class="col nav-menu">
                 <ul class="nav-links justify-content-center">
-                    <li class="logo-for-mobile-navigation"><img src="images/logos/logo_01_white.svg" alt="website logo"></li>
+                    <li class="logo-for-mobile-navigation"><img src="/denden_logo_b.png" alt="DENDEN DENİZCİLİK"></li>
                     <li><a href="{{ route('home')}}" class="nav-link">Anasayfa</a></li>
                     <li class="dd-toggle">
                         <a href="#" class="nav-link">Kurumsal</a>
@@ -48,18 +48,16 @@
             </div>
             <div class="col">
                 <ul class="nav-links justify-content-end">
-                    <!-- Extra Links -->
                     <li class="extra-links">
                         <div class="bracket hidden-xl hidden-sm"></div>
-                        <a href="https://www.youtube.com/" target="_blank" class="visible-lg nav-link" title="youtube"><i class="ti-youtube"></i></a>
-                        <a href="https://www.instagram.com//" target="_blank" class="visible-lg nav-link" title="Instagram"><i class="ti-instagram"></i></a>
-                        <a href="https://www.facebook.com//" target="_blank" class="visible-lg nav-link" title="facebook"><i class="ti-facebook"></i></a>
-                        <a href="{{ route('home')}}" target="_blank" 
-                        class="visible-lg nav-button bg-colored white uppercase bold slow hover-cursor fs-16" title="444 9 409">444 9 409</a>
+                        <a href="https://www.youtube.com/{{ config('settings.youtube')}}" target="_blank" class="visible-lg nav-link" title="youtube"><i class="ti-youtube"></i></a>
+                        <a href="https://www.instagram.com/{{ config('settings.instagram')}}" target="_blank" class="visible-lg nav-link" title="Instagram"><i class="ti-instagram"></i></a>
+                        <a href="https://www.facebook.com/{{ config('settings.facebook')}}" target="_blank" class="visible-lg nav-link" title="facebook"><i class="ti-facebook"></i></a>
+                        <a href="tel:{{ config('settings.telefon1')}}" target="_blank" 
+                        class="visible-lg nav-button bg-colored white uppercase bold slow hover-cursor fs-16" title="444 9 409">{{ config('settings.telefon1')}}</a>
                     </li>
                 </ul>
             </div>
-            <!-- Mobile Nav Button -->
             <div class="mobile-nb">
                 <div class="hamburger-menu">
                     <div class="top-bun"></div>
@@ -68,8 +66,6 @@
                 </div>
             </div>
         </div>
-        <!-- End Row for cols in the nav -->
     </div>
-    <!-- End Navigation container -->
     <div class="mobile-nav-bg"></div>
 </nav>
